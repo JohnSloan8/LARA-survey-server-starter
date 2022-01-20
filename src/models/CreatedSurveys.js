7dd79aaf25d4ab8a1e4f8cd4762f5201b3615715
johnsloan1984 <johnsloan1984@gmail.com>
2022-01-20 13:02:35 +0000

    added date to survey model

--- src/models/CreatedSurveys.js
+++ src/models/CreatedSurveys.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 const CreatedSurveySchema = new mongoose.Schema({
+	date: {
+    type: Date,
+    required: true,
+  },
 	name: {
     type: String,
     required: true,

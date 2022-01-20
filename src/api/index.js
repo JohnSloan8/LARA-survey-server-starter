876107272101157fb571da51fd1260b3dff16318
johnsloan1984 <johnsloan1984@gmail.com>
2022-01-20 11:35:49 +0000

    added createSurveyRoutes

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
 const emojis = require('./emojis');
-
 const router = express.Router();
+const mongoose = require("mongoose")
+const CreatedSurveyModel = require("../models/CreatedSurveys")
+
+mongoose.connect("mongodb+srv://john:Mongojaguar1@laracluster0.wpvro.mongodb.net/LARA-survey?retryWrites=true&w=majority")
+
+router.get("/getCreatedSurveys", (req, res) => {
+	CreatedSurveyModel.find({}, (err, result) => {
+		console.log('result:', result)
+		if (err) {
+			res.json(err)
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+router.post("/createSurvey", async (req, res) => {
+	console.log('req.body:', req.body)
+	const createdSurvey = req.body;
+	const newCreatedSurvey = new CreatedSurveyModel(createdSurvey);
+	await newCreatedSurvey.save();
+	res.json(createdSurvey);
+})
 
 router.get('/', (req, res) => {
   res.json({
